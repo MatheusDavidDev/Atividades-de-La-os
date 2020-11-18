7cf0deb1174a27e96e2e964cf921e2e63b24c01f
matheus <matheusviniciusdavid@gmail.com>
2020-11-17 23:36:12 -0300

    Tabuada criada

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Atividades_de_La_os
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o numero que deseja para fazer a sua tabuada");
+            int numero = int.Parse(Console.ReadLine());
+            int resultado; 
+            Console.WriteLine("-------------");
+            
+            for(var tabuada = 0; tabuada <= 10; tabuada++)
+            {
+                resultado = numero * tabuada;
+                Console.WriteLine(+numero+" x "+tabuada+" = "+resultado);
+            }
+        }
+    }
+}
